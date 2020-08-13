61da889892353058f8bc5b7fd8145ed1b060ba24
Josh Riddle <riddlej9879@yahoo.com>
2020-08-12 19:46:10 -0500

    new commit

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.status(200).json({
     message: `Welcome ${process.env.COHORT}`,
-    fact: process.env.FUN_FACT,
+    fact: process.env.FUN_FACT || "I have no fun facts",
   });
 });
 
